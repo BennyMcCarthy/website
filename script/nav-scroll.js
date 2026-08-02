d72993ac6de894dc79e3d8e065c5731f06ac4816
Ben McCarthy <ben@benjamin.co.nz>
2026-08-02 23:07:57 +1200

    navbar

---
+++ script/nav-scroll.js
@@ -0,0 +1,16 @@
+// https://www.youtube.com/watch?v=Q_XZk5Vnujw
+
+{
+    const nav = document.querySelector(".nav");
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", () => {
+        if (lastScrollY < window.scrollY) {
+            nav.classList.add("nav--hidden")
+        } else {
+            nav.classList.remove("nav--hidden")
+        }
+
+        lastScrollY = window.scrollY; 
+    });
+}
