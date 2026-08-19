c2dbef32b1553efc5787f9a4ac2d2c04918346d3
Ben McCarthy <ben@benjamin.co.nz>
2026-08-19 18:46:26 +1200

    atawhai

---
+++ script/nav-show.js
@@ -0,0 +1,9 @@
+function navShow() {
+  document.getElementById("navFull").style.display = "block";
+}
+
+function navHide() {
+  document.getElementById("navFull").style.display = "none";
+}
+
+onkeydown = (event) => { }
